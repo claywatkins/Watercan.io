89c58c5fec9209a8371b44ff81fbc86bfbd4364b
Clayton Watkins <watkinclayton17@yahoo.com>
2020-08-21 20:20:36 -0700

    Created TableViewController for Plants, Created Custom TableViewCell for Plant Objects.

---
+++ WaterMyPlantsBW/WaterMyPlantsBW/Views/PlantTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlantTableViewCell.swift
+//  WaterMyPlantsBW
+//
+//  Created by Clayton Watkins on 8/21/20.
+//  Copyright © 2020 Clayton Watkins. All rights reserved.
+//
+
+import UIKit
+
+class PlantTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
