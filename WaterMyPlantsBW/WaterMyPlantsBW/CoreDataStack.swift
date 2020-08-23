52ada4e38eee789df38b4dcc5282c1ecfda42c2a
Ronald Huston Jr <rghuston9@gmail.com>
2020-08-23 15:17:07 -0500

    CoreDataStack

--- WaterMyPlantsBW/WaterMyPlantsBW/CoreDataStack.swift
+++ WaterMyPlantsBW/WaterMyPlantsBW/CoreDataStack.swift
@@ -12,4 +12,30 @@ import CoreData
 class CoreDataStack {
     static let shared = CoreDataStack()
     
+    lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Plant")
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("failed to load persistent stores: \(error)")
+            }
+        }
+        //  need to add a data model for this to work
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch {
+                NSLog("error saving to persistent store: \(error)")
+                context.reset()
+            }
+        }
+    }
 }
