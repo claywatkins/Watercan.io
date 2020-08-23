e833742fc863437755c61bdeda73d583d7bbb4cb
Ronald Huston Jr <rghuston9@gmail.com>
2020-08-23 15:02:08 -0500

    CoreDataStack singleton

---
+++ WaterMyPlantsBW/WaterMyPlantsBW/CoreDataStack.swift
@@ -0,0 +1,15 @@
+//
+//  CoreDataStack.swift
+//  WaterMyPlantsBW
+//
+//  Created by ronald huston jr on 8/23/20.
+//  Copyright © 2020 Clayton Watkins. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CoreDataStack {
+    static let shared = CoreDataStack()
+    
+}
