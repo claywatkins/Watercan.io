39b74218c046ac376959c31915f098b45939abb0
Ronald Huston Jr <rghuston9@gmail.com>
2020-08-23 17:37:00 -0500

    Plant+Representation

--- WaterMyPlantsBW/WaterMyPlantsBW/Plant+Convenience.swift
+++ WaterMyPlantsBW/WaterMyPlantsBW/Plant+Convenience.swift
@@ -12,5 +12,38 @@ import CoreData
 extension Plant {
     
     //  create a PlantRepresentation -> turn core data managed Plant object into a PlantReprepresentation object
-    var plantRepresentation: PlantRep
+    var plantRepresentation: PlantRepresentation? {
+        guard let id = id,
+        let nickname = nickname,
+        let species = species,
+            let h20Frequency = h2oFrequency else { return nil }
+        
+        return PlantRepresentation(id: id, nickname: nickname, species: species, h20Frequency: h20Frequency)
+    }
+    
+    //  initializer to create a new managed object in Core Data
+    @discardableResult convenience init(id: Int,
+                                        nickname: String,
+                                        species: String,
+                                        h20Frequency: Int16,
+                                        context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        self.id = id
+        self.nickname = nickname
+        self.species = species
+        self.h20Frequency = h20Frequency
+    }
+    
+    //  now convert a PlantRepresentation object [from JSON] into a managed object
+    @discardableResult convenience init?(plantRepresentation: PlantRepresentation,
+                                         context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        
+        guard let id = plantRepresentation.id else { return nil }
+        
+        self.init(id: id,
+                  nickname: plantRepresentation.nickname,
+                  species: plantRepresentation.species,
+                  h20Frequency: plantRepresentation.h20Frequency,
+                  context: context)
+    }
 }
