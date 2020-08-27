5ff2f5c18a0ae680728207eaf71b143689645c76
IanFrench <iangordonfrench@gmail.com>
2020-08-26 22:05:54 -0400

    Added stubs, and two successful tests

--- WaterMyPlantsBW/WaterMyPlantsBWTests/WaterMyPlantsBWTests.swift
+++ WaterMyPlantsBW/WaterMyPlantsBWTests/WaterMyPlantsBWTests.swift
@@ -11,24 +11,55 @@ import XCTest
 
 class WaterMyPlantsBWTests: XCTestCase {
 
-//    override func setUpWithError() throws {
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//    }
-//
-//    override func tearDownWithError() throws {
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//    }
-//
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-//
-//    func testPerformanceExample() throws {
-//        // This is an example of a performance test case.
-//        measure {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
 
+    func testSignUp() {
+
+    }
+
+    func testSignIn() {
+
+    }
+
+
+
+    func testCancelSignUpNotification() {
+
+    }
+
+    // PlantController
+    func testFetchEntriesFromServer() {
+
+        
+    }
+
+    // PlantTableViewHelper
+    func testCancelTapped() {
+        
+    }
+    
+ // PlantTableViewHelper
+    func testPlantWasAdded() {
+        let newPlant = Plant(nickname: "Tree", species: "Elm", h2ofrequency: "Daily")
+        XCTAssert(newPlant.nickname == "Tree")
+        XCTAssert(newPlant.species == "Elm")
+        XCTAssertNotNil(newPlant.h2ofrequency)
+    }
+
+    func testConvertPlantToRep() {
+           let testPlant = Plant(nickname: "Fiddlehead", species: "Fern", h2ofrequency: "Daily")
+           let representation = testPlant.plantRepresentation
+           XCTAssertNotNil(representation)
+           XCTAssert(representation?.nickname == "Fiddlehead")
+           XCTAssert(representation?.species == "Fern")
+
+       }
+//Popup
+    func testAddPhotoPickerController() {
+
+    }
+
+    func testImagePickerController() {
+
+
+    }
 }
