2405b55ff2adbcdc62ad0088a31f1f312efa4102
IanFrench <iangordonfrench@gmail.com>
2020-08-26 22:22:33 -0400

    Signin / Signup tests added

--- WaterMyPlantsBW/WaterMyPlantsBWTests/WaterMyPlantsBWTests.swift
+++ WaterMyPlantsBW/WaterMyPlantsBWTests/WaterMyPlantsBWTests.swift
@@ -12,12 +12,25 @@ import XCTest
 class WaterMyPlantsBWTests: XCTestCase {
 
 
-    func testSignUp() {
+    var newUser: String {
+        return "Ian1"
+    }
 
+    func testSignUp() {
+        let plantController = PlantController()
+               let testUser1 = User(username: newUser, password: "ian123", phonenumber: "121-212-1212")
+               print("User \(testUser1.username) has been created ")
+               plantController.signUp(with: testUser1, completion: { error in
+                   XCTAssertNil(error)
+               })
     }
 
     func testSignIn() {
-
+        let plantController = PlantController()
+        let testUser2 = User(username: "ian2", password: "igf456", phonenumber: "223-232-2323")
+               plantController.signIn(with: testUser2, completion: { error in
+                   XCTAssertNil(error)
+               })
     }
 
 
